ed3c103796b34c9a49c98cdba5b20b5fa7324401
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-24 22:28:20 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_exception_handler do
+  name 'Redmine Exception Handler plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
