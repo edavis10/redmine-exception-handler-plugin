cea189d0ec41a494dd6b9af922c7210d15ecd576
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-24 23:18:08 -0700

    Loading exception_notification plugin and monkey patching it's delivery method

--- init.rb
+++ init.rb
@@ -1,8 +1,46 @@
 require 'redmine'
 
+Dir[File.join(directory,'vendor','plugins','*')].each do |dir|
+  path = File.join(dir, 'lib')
+  $LOAD_PATH << path
+  Dependencies.load_paths << path
+  Dependencies.load_once_paths.delete(path)
+end
+
 Redmine::Plugin.register :redmine_exception_handler do
   name 'Redmine Exception Handler plugin'
   author 'Author name'
   description 'This is a plugin for Redmine'
   version '0.0.1'
+  
+  settings :default => {
+    'exception_handler_recipients' => 'you@example.com',
+    'exception_handler_sender_address' => 'Application Error <redmine@example.com>',
+    'exception_handler_prefix' => '[ERROR]'
+  }, :partial => 'settings/settings'
+  
 end
+
+require_dependency 'exception_notifier'
+
+module RedmineExceptionNotifierPatch
+  def self.included(target)
+    target.extend(ClassMethods)
+    target.send(:include, InstanceMethods)
+  end
+
+  module ClassMethods
+    self.template_root = "#{File.join(File.dirname(__FILE__),'vendor','plugins','exception_notification','view')}"
+  end
+    
+  module InstanceMethods
+    def exception_notification_with_database(exception, controller, request, data={}, &block)
+      exception_notification_without_database(exception, controller, request, data, &block)
+    end
+
+  end
+  
+end
+ExceptionNotifier.send(:include, RedmineExceptionNotifierPatch)
+ExceptionNotifier.send(:alias_method_chain, :exception_notification, :database)
+ActionController::Base.send(:include, ExceptionNotifiable)
