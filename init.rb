267c54e023acb119bef0559e08fb1a9064ba5706
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-24 20:51:44 -0800

    Fixed the incompatibility with rails 2.2.x #2188

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Dir[File.join(directory,'vendor','plugins','*')].each do |dir|
   path = File.join(dir, 'lib')
   $LOAD_PATH << path
-  Dependencies.load_paths << path
-  Dependencies.load_once_paths.delete(path)
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
 end
 
 Redmine::Plugin.register :exception_handler do
